c9a27d42475cd5daeab0595a448c86064ee69efb
David Crispell <davidglenrc@gmail.com>
2025-07-17 16:21:36 -0600

    fixed login

--- ProjectSourceCode/SRC/init_data/create.sql
+++ ProjectSourceCode/SRC/init_data/create.sql
@@ -1,3 +1,9 @@
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  email TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);
 --Table for storing workout logs
 CREATE TABLE workoutlogs (
   id INT PRIMARY KEY

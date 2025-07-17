34fd54e6bbaa8c253d084c4322c1f8a2ed86a121
David Crispell <davidglenrc@gmail.com>
2025-07-17 16:04:21 -0600

    added joe commits to test repo

--- ProjectSourceCode/SRC/init_data/create.sql
+++ ProjectSourceCode/SRC/init_data/create.sql
@@ -1,6 +1,34 @@
-CREATE TABLE IF NOT EXISTS users (
-  id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
-  email TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL
+--Table for storing workout logs
+CREATE TABLE workoutlogs (
+  id INT PRIMARY KEY
+  workoutname VARCHAR(100) NOT NULL,
+  date DATE NOT NULL, 
+  workoutduration SMALLINT NOT NULL,
+  exercise_categories VARCHAR(100) NOT NULL
+  );
+
+CREATE table cardiologs (
+  id INT PRIMARY KEY,
+  workoutname VARCHAR(100) NOT NULL,
+  date DATE NOT NULL,
+  workoutduration SMALLINT NOT NULL,
+  exercise_categories VARCHAR(100) NOT NULL,
+  distance FLOAT NOT NULL
+);
+
+CREATE TABLE weigthliftinglogs (
+  id INT PRIMARY KEY,
+  workoutname VARCHAR(100) NOT NULL,
+  exercise_name VARCHAR(100) NOT NULL,
+  sets SMALLINT NOT NULL,
+  reps SMALLINT NOT NULL,
+  weight FLOAT NOT NULL
+);
+
+CREATE TABLE bodyweightlogs (
+  id INT PRIMARY KEY,
+  workoutname VARCHAR(100) NOT NULL,
+  exercise_name VARCHAR(100) NOT NULL,
+  sets SMALLINT NOT NULL,
+  reps SMALLINT NOT NULL
 );
---
+++ ProjectSourceCode/SRC/init_data/insert.sql
@@ -0,0 +1,46 @@
+--initialize the databases with default values for workout logs.
+
+INSERT INTO weigthliftinglogs
+  (
+    id,
+    workoutname,
+    date,
+    workoutduration,
+    workoutlog_id,
+    exercise_categories
+    distance,
+  )
+VALUES
+    (
+    
+    );
+
+INSERT INTO cardiologs
+  (
+    id,
+    workoutname,
+    date,
+    workoutduration,
+    workoutlog_id,
+    exercise_categories
+    distance
+  )
+VALUES
+  (
+
+  );
+
+
+INSERT INTO bodyweightlogs
+    (
+        id,
+        workoutname,
+        date,
+        workoutduration,
+        workoutlog_id,
+        exercise_categories
+    )
+VALUES
+    (
+    
+    );
